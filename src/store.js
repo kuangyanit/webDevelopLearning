93eec61009fc3cb015997f225e4f1a3559b4837e
kuangyan <faq366@163.com>
2018-10-10 19:24:03 +0800

    vuex学习

--- src/store.js
+++ src/store.js
@@ -5,12 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    com_pageName_mutations: null,
+    com_pageName_actions: null
   },
+  //只支持同步
   mutations: {
-
+    SET_COM_PAGENAME_MUTATIONS_MUTATIONSTYLE(state, value){
+      state.com_pageName_mutations = value 
+    },
+    SET_COM_PAGENAME_ACTIONS_MUTATIONSTYLE(state, value){
+      state.com_pageName_actions = value
+    }
   },
+  //同步异步均支持
   actions: {
-
+    SET_COM_PAGENAME_ACTIONS_ACTIONSTYLE({ commit, rootState}, value){
+      commit('SET_COM_PAGENAME_ACTIONS_MUTATIONSTYLE', value)
+    }
   }
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page, named {{ com_pageName_mutations }}</h1>
+    <input v-model="com_pageName_mutations"/>
+
+    <h1>This is an about page, named {{ com_pageName_actions }}</h1>
+    <input v-model="com_pageName_actions"/>
   </div>
 </template>
+<script>
+
+export default{
+  name: 'about',
+  data(){
+    return {
+      pageName: '',
+    }
+  },
+  methods: {
+    
+  },
+  computed: {
+    com_pageName_mutations: {
+      get(){
+        return this.$store.state.com_pageName_mutations 
+      },
+      set(value){
+        this.$store.commit('SET_COM_PAGENAME_MUTATIONS_MUTATIONSTYLE', value);
+      }
+    },
+
+    com_pageName_actions: {
+      get(){
+        return this.$store.state.com_pageName_actions 
+      },
+      set(value){
+         this.$store.dispatch('SET_COM_PAGENAME_ACTIONS_ACTIONSTYLE',value)
+      }
+    }
+  }
+
+}
+
+</script>
+
+
